fe074704af441805d335ed7ed203b07e02aa488c
Mohammed Aashik <aashikdevelop@gmail.com>
2026-09-19 23:44:09 +0400

    Remove Helmet Middleware from Main Application File
    - Commented out the Helmet middleware integration in main.ts to disable security headers.
    - This change may be temporary for testing or debugging purposes, pending further review of security requirements.

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,12 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import helmet from 'helmet';
+// import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use(helmet());
+  // app.use(helmet());
 
   app.useGlobalPipes(
     new ValidationPipe({
