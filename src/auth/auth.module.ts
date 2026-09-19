d64b5b92f50b5cf6f72e4ea09b3cf8100cdfe747
Mohammed Aashik <aashikdevelop@gmail.com>
2026-09-19 15:23:23 +0400

    Enhance AuthModule with Passport integration and JWT strategy
    - Added PassportModule with a default JWT strategy for improved authentication handling.
    - Registered JwtStrategy as a provider in AuthModule to facilitate JWT validation.
    - Exported PassportModule for use in other modules.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   imports: [
+    PassportModule.register({
+      defaultStrategy: 'jwt',
+    }),
+
     JwtModule.register({
       global: true,
       secret: process.env.JWT_ACCESS_SECRET,
@@ -15,6 +21,7 @@ import { JwtModule } from '@nestjs/jwt';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
+  exports: [PassportModule],
 })
 export class AuthModule {}
